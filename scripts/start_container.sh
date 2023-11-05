13536ac2a3989b9960fea259566c41c145ccb080
Anandhi <50393414+Anandhi-23@users.noreply.github.com>
2023-11-05 19:28:32 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull docker pull anandhi23/python-django-app:latest
+docker pull anandhi23/python-django-app:latest
 
 # Run the Docker image as a container
 docker run -d -p 8000:8000 anandhi23/python-django-app:latest
