f5ffae0990d859ed2d498cb07323af5b81ffeab3
Anandhi <50393414+Anandhi-23@users.noreply.github.com>
2023-11-05 20:27:52 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -3,7 +3,13 @@ set -e
 
 # Stop the running container (if any)
 containerID=$(docker ps --format "{{.ID}}" | awk -F, '{print $1}')
+echo "removing container"
 docker rm -f $containerID
+
+
+
+
+
 
 
 
